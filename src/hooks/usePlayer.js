b0c862b8b6ac09d0397134eaef9411d0a5af5924
nurgnc <gencbedriyenur@gmail.com>
2021-12-14 13:57:17 +0300

    completed custom hooks

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,5 +1,7 @@
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
 import {randomTetromino} from '../tetraminos'
+
+import {STAGE_WIDTH} from '../gameHelpers'
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
@@ -11,9 +13,18 @@ export const usePlayer = () => {
     const updatePlayerPos = ({ x, y, collided}) => {
         setPlayer(prev => ({ 
             ...prev,
-            pos: {x: (prev.pos.x += x), y: (prev.pos.y += y)}        
+            pos: {x: (prev.pos.x += x), y: (prev.pos.y += y)},
+            collided,
         }))
     }
 
-    return [player]; 
+    const resetPlayer = useCallback(() => {
+        setPlayer({
+            pos: {x: STAGE_WIDTH / 2 - 2, y: 0},
+            tetromino: randomTetromino().shape,
+            collided: false,
+        })
+    }, [])
+
+    return [player, updatePlayerPos, resetPlayer]; 
 }
